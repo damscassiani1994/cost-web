65555398637d6f0876c14ad6f1437b269508edae
DAMASO SALGADO CASSIANI <damsc1994@gmail.com>
2019-08-11 10:12:05 -0500

    quitar espacio de app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { environment } from '../environments/environment';
 import { EffectsModule } from '@ngrx/effects';
 
-
 @NgModule({
   declarations: [
     AppComponent
