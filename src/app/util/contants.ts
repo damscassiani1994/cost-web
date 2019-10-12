06e2ce2088a6ba027e2c44fa15c1a58cf55755ca
DAMASO SALGADO CASSIANI <damsc1994@gmail.com>
2019-10-12 18:27:22 -0500

    creacion del formulario info-user

--- src/app/util/contants.ts
+++ src/app/util/contants.ts
@@ -1,6 +1,6 @@
 export const data = {
   userToken: 'ustkn',
   userObject: 'usrObj',
-  apiUser: 'http://localhost:1801/api/user',
+  apiUser: 'http://CostUserServices-env.kdsd5guixm.us-east-2.elasticbeanstalk.com/api/user',
   apiCost: 'http://localhost:1802/api/cost'
 };
